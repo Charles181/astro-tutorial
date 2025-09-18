419d20426159bf0d8ceae1e88138a0410cbc35ef
Carlos Santillan <csantillanj@gmail.com>
2025-09-18 17:16:03 -0500

    Added theme toggle, css for dark theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://starlit-genie-77e982.netlify.app/"    
+  site: "https://starlit-genie-77e982.netlify.app/",
+  integrations: [preact()]
 });
